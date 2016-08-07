3bf9a68157c5d282166095522e1a7762bfc9c8a5
Troy Lamerton <troy.lamerton@gmail.com>
2016-08-07 14:53:38 +1200

    Change double quotes to single quotes

--- client/index.js
+++ client/index.js
@@ -29,9 +29,9 @@ document.addEventListener('DOMContentLoaded', () => {
 const isChromium = window.chrome,
   winNav = window.navigator,
   vendorName = winNav.vendor,
-  isOpera = winNav.userAgent.indexOf("OPR") > -1,
-  isIEedge = winNav.userAgent.indexOf("Edge") > -1,
-  isIOSChrome = winNav.userAgent.match("CriOS");
+  isOpera = winNav.userAgent.indexOf('OPR') > -1,
+  isIEedge = winNav.userAgent.indexOf('Edge') > -1,
+  isIOSChrome = winNav.userAgent.match('CriOS');
 
 if (isChromium !== null && isChromium !== undefined && vendorName === 'Google Inc.' && isOpera === false && isIEedge === false) {
   // is Google Chrome

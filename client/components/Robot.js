8db2bb319cc3e8299e940dc0c27d3c2bda9a0bc9
Troy <troy.lamerton@gmail.com>
2025-04-25 22:04:00 +1200

    refactor: improve performance of idle animation by using css

--- client/components/Robot.js
+++ client/components/Robot.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import { Motion, spring } from 'react-motion'
-import Sound from 'react-sound'
-import { ReactMotionLoop } from 'react-motion-loop'
 import WinContainer from '../containers/WinContainer'
 import classNames from 'classnames'
+import { JUMP_UP, MOVE_FORWARD, SOUND, TOGGLE_SOUND, TURN_LEFT, TURN_RIGHT } from '../reducers/action'
 
 class Robot extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      scale: 90,
+    }
+  }
+
   calcCenter () {
     const y = this.props.robot.positionY.toString()
     const x = this.props.robot.positionX.toString()
@@ -23,100 +29,87 @@ class Robot extends Component {
     window.onresize = null
   }
 
+  /** Set scale based on robot props */
+  componentDidUpdate (_prevProps) {
+    const defaultScale = 90;
+    if (!this.props.robot.isAlive) {
+      if (this.state.scale !== 10) {
+        this.setState({ scale: 10 })
+      }
+      return;
+    }
+    const commandExecuted = this.props.commandQueue[this.props.executeCommandIndex - 1]
+    switch (commandExecuted) {
+      case JUMP_UP:
+        if (this.state.scale !== 110) {
+          this.setState({ scale: 110 })
+        }
+        break;
+      case MOVE_FORWARD:
+        if (this.state.scale !== defaultScale) {
+          this.setState({ scale: defaultScale})
+        }
+        break;
+      case TURN_LEFT, TURN_RIGHT, TOGGLE_SOUND, SOUND:
+        // do nothing
+        break;
+      default: {
+        // when you retry etc, reset robot scale
+        if (this.state.scale !== defaultScale) {
+          this.setState({ scale: defaultScale})
+        }
+      }
+    }
+  }
+
   render () {
     const centerPoints = this.calcCenter()
-    let size = 80
-    const positioning = size / 2
-    let dampingStrength = 1
-    let stiffnessStrength = 100
     let precisionStrength = 1
-    let scale = 100
     let opacity = 1.0
-    const commandExecuted = this.props.commandQueue[this.props.executeCommandIndex - 1]
-    if (commandExecuted === 'JUMP_UP') {
-      if (!this.props.robot.isAlive) {
-        precisionStrength = 0.1
-        stiffnessStrength = 300
-        scale = 0
-        opacity = 0.1
-      }
-      dampingStrength = 2
-      size = size + 20
-    } else if (commandExecuted === 'TURN_RIGHT' || commandExecuted === 'TURN_LEFT') {
-      size = size + 5
-    } else if (this.props.levelWon === true) {
-      opacity = 0
-    } else if (!this.props.robot.isAlive) {
+    if (!this.props.robot.isAlive) {
       precisionStrength = 0.1
-      stiffnessStrength = 300
-      scale = 0
-      opacity = 0.1
+      opacity = 0.4
     }
     return (
       <div>
         <Motion
           defaultStyle={{
-            scale,
+            scale: this.state.scale,
             x: centerPoints[0],
             y: centerPoints[1],
             rot: 0
-          }} style={{
-            scale: spring(scale, { precision: precisionStrength }),
+          }}
+          style={{
+            scale: spring(this.state.scale, { precision: precisionStrength }),
             x: spring(centerPoints[0]),
             y: spring(centerPoints[1]),
             rot: spring(this.props.robot.direction)
           }}
-        >
-          {value => <div
-            className={this.props.levelWon ? classNames('b3-container-animation') : classNames('')} style={{
-              height: value.scale,
-              width: value.scale,
-              position: 'absolute',
-              top: value.y - 70,
-              left: value.x - 55,
-              transform: `rotate(${value.rot}deg)`
-            }}
-                    >
-            <ReactMotionLoop
-              styleFrom={{
-                opacity: spring(opacity),
-                width: spring(size, {
-                  stiffness: stiffnessStrength,
-                  damping: dampingStrength,
-                  precision: precisionStrength
-                })
-              }} styleTo={{
-                opacity: spring(opacity),
-                width: spring(size + 2, {
-                  stiffness: stiffnessStrength,
-                  damping: dampingStrength,
-                  precision: precisionStrength
-                })
-              }}
-            >
-              {style => <img src='/resources/images/b3-robot.svg' className={classNames('b3-robot')} style={style} />}
-            </ReactMotionLoop>
-            <ReactMotionLoop
-              styleFrom={{
-                opacity: spring(opacity),
-                width: spring(size, {
-                  stiffness: stiffnessStrength,
-                  damping: dampingStrength,
-                  precision: precisionStrength
-                })
-              }} styleTo={{
-                opacity: spring(opacity),
-                width: spring(size - 2, {
-                  stiffness: stiffnessStrength,
-                  damping: dampingStrength,
-                  precision: precisionStrength
-                })
-              }}
-            >
-              {style => <img src='/resources/images/shadow.svg' className={this.props.levelWon ? classNames('shadow-animation') : classNames('shadow')} style={style} />}
-            </ReactMotionLoop>
+          children={value => <div>
+            <div
+              className={classNames('robot-container', this.props.levelWon ? 'b3-container-animation' : '')}
+              style={{
+                height: value.scale,
+                width: value.scale,
+                top: value.y - 12,
+                left: value.x,
+                transform: `translate(-50%, -50%)`,
+              }}>
+              <img src="/resources/images/shadow.svg" className={classNames('shadow', { 'shadow-animation': this.props.levelWon })} />
+            </div>
+            <div
+              className={classNames('robot-container', this.props.levelWon ? 'b3-container-animation' : '')}
+              style={{
+                height: value.scale,
+                width: value.scale,
+                top: value.y - 12 - value.scale/2,
+                left: value.x - value.scale/2,
+                rotate: `${value.rot}deg`,
+              }}>
+              <img src="/resources/images/b3-robot.svg" className="b3-robot" style={{opacity}} />
+            </div>
           </div>}
-        </Motion>
+        />
         <WinContainer />
       </div>
     )

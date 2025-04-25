1be2bd7b2e33b4582bd0080029862e1d836bfc86
Troy <troy.lamerton@gmail.com>
2025-04-25 22:13:48 +1200

    refactor: bg sound off by default

--- client/components/Backgroundtrack.js
+++ client/components/Backgroundtrack.js
@@ -11,7 +11,6 @@ export default class Backgroundtrack extends Component {
       theme: 0,
       currentSong: songs[0],
       position: 0,
-      volume: 25,
       playStatus: Sound.status.STOPPED
     }
   }
@@ -30,26 +29,27 @@ export default class Backgroundtrack extends Component {
   }
 
   componentDidMount () {
-    const cookies = cookie.load('sound')
+    let cookies = cookie.load('sound')
     if (cookies === undefined) {
-      cookie.save('sound', 'ON')
-      this.setState({ playStatus: Sound.status.PLAYING })
-    } else if (cookies === 'ON') {
-      this.setState({ playStatus: Sound.status.PLAYING })
-    } else {
-      this.setState({ playStatus: Sound.status.STOPPED })
+      cookie.save('sound', 'OFF')
     }
+    if (cookies === 'ON') {
+      this.setState({ playStatus: Sound.status.PLAYING })
+      return
+    }
+    this.setState({ playStatus: Sound.status.STOPPED })
   }
 
   componentDidUpdate () {
     let newTheme = 0
-    if (this.props.currentLevel >= 1 && this.props.currentLevel < 6) {
+    const { currentLevel } = this.props
+    if (currentLevel >= 1 && currentLevel < 6) {
       newTheme = 0
-    } else if (this.props.currentLevel >= 6 && this.props.currentLevel < 11) {
+    } else if (currentLevel >= 6 && currentLevel < 11) {
       newTheme = 1
-    } else if (this.props.currentLevel >= 11 && this.props.currentLevel < 16) {
+    } else if (currentLevel >= 11 && currentLevel < 16) {
       newTheme = 2
-    } else if (this.props.currentLevel >= 16 && this.props.currentLevel < 21) {
+    } else if (currentLevel >= 16 && currentLevel < 21) {
       newTheme = 3
     } else {
       newTheme = 4
@@ -59,9 +59,9 @@ export default class Backgroundtrack extends Component {
       this.setState({ theme: newTheme, currentSong: songs[newTheme] })
     }
 
-    const cookies = cookie.load('sound')
+    const soundCookie = cookie.load('sound')
     const isAlreadySet = this.state.playStatus === Sound.status.PLAYING
-    if (cookies === 'ON') {
+    if (soundCookie === 'ON') {
       if (isAlreadySet) return
       this.setState({ playStatus: Sound.status.PLAYING })
     } else {
@@ -72,15 +72,14 @@ export default class Backgroundtrack extends Component {
 
   render () {
     return (
-      <div>
-        <Sound
-          url={this.state.currentSong.url}
-          playStatus={this.state.playStatus}
-          playFromPosition={this.state.position}
-          volume={this.state.volume}
-          onFinishedPlaying={() => this.setState({ playStatus: Sound.status.PLAYING })}
-        />
-      </div>
+      <Sound
+        url={this.state.currentSong.url}
+        playStatus={this.state.playStatus}
+        playFromPosition={this.state.position}
+        volume={25}
+        loop={true}
+        onFinishedPlaying={() => this.setState({ playStatus: Sound.status.PLAYING })}
+      />
     )
   }
 }

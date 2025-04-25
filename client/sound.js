fe1935780f1dae4726a62bf5b22b27b0095a7fbb
Troy <troy.lamerton@gmail.com>
2025-04-25 17:40:50 +1200

    chore: remove unused code

--- client/sound.js
+++ client/sound.js
@@ -2,26 +2,22 @@ export default [
   {
     url: './resources/sound/background_loops/one.mp3',
     title: 'background1',
-    duration: 0
   },
   {
     url: './resources/sound/background_loops/two.mp3',
     title: 'background2',
-    duration: 0
   },
   {
     url: './resources/sound/background_loops/three.mp3',
     title: 'background3',
-    duration: 0
   },
   {
      url: './resources/sound/background_loops/four.mp3',
      title: 'background4',
-     duration: 0
+
   } ,
   {
     url: './resources/sound/background_loops/five.mp3',
     title: 'background5',
-    duration: 0
   } 
 ]
